23b79e910e5d25804e97e1a5c5e48b0fb2f026e4
Olle Ekdala <ekdalaolle@gmail.com>
2022-09-15 18:59:59 +0200

    some testing with sockets (copypaste)

--- A2/mathserver/src/client.c
+++ A2/mathserver/src/client.c
@@ -1,43 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int connect(char *ip_addr, int port_no);
-int command();
-void help_text();
-char *send(char *command);
+// int connect(char *ip_addr, int port_no);
+// int command();
+// void help_text();
+// char *send(char *command);
 
 // Command line options to handle:
 // -p port
 // -ip address
 
-void main(int argc, char **argv)
+void main(int argc, char *argv[])
 {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    struct sockaddr_in server_address;
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = atoi(argv[1]);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    int connect_status = connect(sock, (struct sockaddr*)&server_address, sizeof(server_address));
+
+    if (connect_status == -1) {
+        printf("Error\n");
+    } 
+    else {
+        char strData[255];
+        recv(sock, strData, sizeof(strData), 0);
+
+        printf("message: %s\n", strData);
+    }
     // while (1) {}
-    connect("", 0);
-    command();
+
+    // command();
 }
 
-// Connect to server
-int connect(char *ip_addr, int port_no)
-{
-    printf("Connected to server\n");
-    return 0;
-}
+// // Connect to server
+// int connect(char *ip_addr, int port_no)
+// {
+//     printf("Connected to server\n");
+//     return 0;
+// }
 
-// Take user input, send message, present solution
-int command()
-{
-    char *solution = "-", *msg = "-";
+// // Take user input, send message, present solution
+// int command()
+// {
+//     char *solution = "-", *msg = "-";
 
-    printf("Enter a command for the server: ");
-    printf("\n");
-    solution = send(msg);
-    printf("Recieved the solution: %s\n", solution);
-    return 0;
-}
+//     printf("Enter a command for the server: ");
+//     printf("\n");
+//     solution = send(msg);
+//     printf("Recieved the solution: %s\n", solution);
+//     return 0;
+// }
 
-// Send a command to server
-char *send(char *msg)
-{
-    return "-";
-}
+// // Send a command to server
+// char *send(char *msg)
+// {
+//     return "-";
+// }
--- A2/mathserver/src/server.c
+++ A2/mathserver/src/server.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int connect(int port_no);
-void init(int argc, char **argv);
-char *receive_command(char *msg);
+// int connect(int port_no);
+// void init(int argc, char **argv);
+// char *receive_command(char *msg);
 
 // Default values
 int port_no = -1;
@@ -17,39 +19,63 @@ char *strategy = "-";
 // -s strategy  (Specify the request handling strategy : fork, muxbasic, or muxscale)
 
 // Server should handle concurrent clients
-void main(int argc, char **argv)
+void main(int argc, char *argv[])
 {
-    // while (1) {}
-    init(argc, argv);
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // char msg[255] = "This message is from the server\n";
+    char msg[255];
+    strcpy(msg, argv[2]);
+    
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = atoi(argv[1]);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
+    listen(server_socket, 1);
     printf("Listening for clients...\n");
-    connect(0);
-    char *solution = receive_command("-");
-    printf("Sending solution: %s\n", solution);
-}
-
-// Set values according to command line flags
-void init(int argc, char **argv)
-{
-    // set port_no, daemon, strategy if needed
-    if (daemon)
-    {
-        char *pid = "-";
-        printf("Daemon process ID: %s\n", pid);
+    int client_socket;
+    while (client_socket = accept(server_socket, NULL, NULL)) {
+        printf("Sending message to client");
+        // Action
+        send(client_socket, msg, sizeof(msg), 0);
+        // Instead of send here, could be a call to fork
+        
+        close(client_socket);
     }
-}
 
-// Connect with a client
-int connect(int port_no)
-{
-    char *client = "-";
-    printf("Connected with client %s\n", client);
     return 0;
+    // while (1) {}
+    // init(argc, argv);
+    // connect(0);
+    // char *solution = receive_command("-");
+    // printf("Sending solution: %s\n", solution);
 }
 
-// Receive command from client and execute
-char *receive_command(char *msg)
-{
-    char *fname = "-", *client = "-";
-    printf("Client %s commanded:  %s\n", client, msg);
-    return fname;
-}
+// // Set values according to command line flags
+// void init(int argc, char **argv)
+// {
+//     // set port_no, daemon, strategy if needed
+//     if (daemon)
+//     {
+//         char *pid = "-";
+//         printf("Daemon process ID: %s\n", pid);
+//     }
+// }
+
+// // Connect with a client
+// int connect(int port_no)
+// {
+//     char *client = "-";
+//     printf("Connected with client %s\n", client);
+//     return 0;
+// }
+
+// // Receive command from client and execute
+// char *receive_command(char *msg)
+// {
+//     char *fname = "-", *client = "-";
+//     printf("Client %s commanded:  %s\n", client, msg);
+//     return fname;
+// }
