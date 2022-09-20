06de986a27fd7fcad34cee1b7c532582acc7f997
Felicia de Mander <fede16@student.bth.se>
2022-09-21 00:51:08 +0200

    client/server messages back and forth but server exits as soon as client exits, outer while loop doesn't help

--- A2/mathserver/src/client.c
+++ A2/mathserver/src/client.c
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+void validate_args(int argc, char *argv[]);
 // int connect(char *ip_addr, int port_no);
 // int command();
 // void help_text();
@@ -12,9 +13,10 @@
 // -p port
 // -ip address
 
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    validate_args(argc, argv);
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in server_address;
 
@@ -22,20 +24,35 @@ void main(int argc, char *argv[])
     server_address.sin_port = atoi(argv[1]);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
-    int connect_status = connect(sock, (struct sockaddr*)&server_address, sizeof(server_address));
+    int connect_status = connect(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
 
-    if (connect_status == -1) {
+    if (connect_status == -1)
+    {
         printf("Error\n");
-    } 
-    else {
-        char strData[255];
-        recv(sock, strData, sizeof(strData), 0);
-
-        printf("message: %s\n", strData);
+        exit(1);
     }
-    // while (1) {}
 
-    // command();
+    int running = 1;
+    while (running)
+    {
+        char strData[255];
+        printf("Enter a command for the server: ");
+        fgets(strData, 255, stdin);
+        send(server_socket, strData, sizeof(strData), 0);
+
+        recv(server_socket, strData, sizeof(strData), 0);
+        printf("Received the solution: %s", strData);
+    }
+    exit(0);
+}
+
+void validate_args(int argc, char *argv[])
+{
+    if (argc < 2 || argc > 5)
+    {
+        printf("Error\n");
+        exit(1);
+    }
 }
 
 // // Connect to server
--- A2/mathserver/src/server.c
+++ A2/mathserver/src/server.c
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+void validate_args(int argc, char *argv[]);
 // int connect(int port_no);
 // void init(int argc, char **argv);
 // char *receive_command(char *msg);
@@ -19,38 +20,61 @@ char *strategy = "-";
 // -s strategy  (Specify the request handling strategy : fork, muxbasic, or muxscale)
 
 // Server should handle concurrent clients
-void main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    while (1) // only runs once
+    {
+        validate_args(argc, argv);
+        int server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    // char msg[255] = "This message is from the server\n";
-    char msg[255];
-    strcpy(msg, argv[2]);
-    
-    struct sockaddr_in server_address;
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = atoi(argv[1]);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+        // char msg[255] = "This message is from the server\n";
+        // char msg[255];
+        // strcpy(msg, argv[2]);
 
-    bind(server_socket, (struct sockaddr*)&server_address, sizeof(server_address));
-    listen(server_socket, 1);
-    printf("Listening for clients...\n");
-    int client_socket;
-    while (client_socket = accept(server_socket, NULL, NULL)) {
-        printf("Sending message to client");
-        // Action
-        send(client_socket, msg, sizeof(msg), 0);
-        // Instead of send here, could be a call to fork
-        
-        close(client_socket);
+        struct sockaddr_in server_address;
+        server_address.sin_family = AF_INET;
+        server_address.sin_port = atoi(argv[1]);
+        server_address.sin_addr.s_addr = INADDR_ANY;
+
+        bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address));
+        listen(server_socket, 4096);
+        printf("Listening for clients...\n");
+
+        int client_socket;
+        while (client_socket = accept(server_socket, NULL, NULL))
+        {
+            while (1)
+            {
+                char msg[255];
+                recv(client_socket, msg, sizeof(msg), 0);
+                send(client_socket, msg, sizeof(msg), 0);
+                // Instead of send here, could be a call to fork
+                // close(client_socket);
+            }
+        }
+        // when client exits, server exits
     }
-
     return 0;
-    // while (1) {}
+
     // init(argc, argv);
     // connect(0);
     // char *solution = receive_command("-");
     // printf("Sending solution: %s\n", solution);
+}
+
+void validate_args(int argc, char *argv[])
+{
+    // printf("Validating args (%d): ", argc);
+    // for (int i = 0; i < argc; i++)
+    // {
+    //     printf("'%s' ", argv[i]);
+    // }
+    // printf("\n");
+    if (argc < 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
 }
 
 // // Set values according to command line flags
