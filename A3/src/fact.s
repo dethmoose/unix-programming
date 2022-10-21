54c3f29aa515bdebb03858eb5385305f101085a1
Felicia de Mander <fede16@student.bth.se>
2022-10-21 13:41:27 +0200

    fact

--- A3/src/fact.s
+++ A3/src/fact.s
@@ -1,8 +1,17 @@
 # ar - program
-
-.data
-.text
-.global fact
-
+    .data
+    .text
+    .global fact
 
 fact:
+    cmpq    $1,%rdi         # if n>1
+    jle     base
+    pushq   %rdi            # lägg anropsvärde på stacken
+    decq    %rdi            # räkna ned värdet med 1
+    call    fac             # temp = fakultet av (n-1)
+    popq    %rdi            # hämta från stack
+    imulq   %rdi,%rax       # return n*temp
+    ret
+base:
+    movq    $1,%rax         # else return 1
+    ret
