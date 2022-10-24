3a4c32f49666bfef192bb4b13f522fd161050032
Felicia de Mander <fede16@student.bth.se>
2022-10-24 14:14:41 +0200

    x86-64-driver.sh syscall epilogue
    http://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/

--- A3/x86-64-driver.sh
+++ A3/x86-64-driver.sh
@@ -1,13 +1,24 @@
 in_filename=$1  
 filename="output.s"
 
+# Create prologue (.bss and .text segments)
 ALPHA="a b c d e f g h i j k l m n o p q r s t u v w x y z"
-echo ".bss" > $filename
+echo -e "\t.bss" > $filename
 for a in $ALPHA
 do
-    echo "$a:   .quad   0" >> $filename
+    echo -e "$a:\t.quad\t0" >> $filename
 done
+echo -e "\n\t.text"         >> $filename
+echo -e "\t.global\tmain\n" >> $filename
+echo    "main:"             >> $filename
 
 # Command to parse calc file, and compile...
 
-echo "  exit" >> $filename
+# Executing with test file
+(./bin/calc3b.exe < ./testprogs/gcd.calc) >> $filename
+
+# Create epilogue
+echo    "lExit:"             >> $filename
+echo -e "\tmovq\t\$60,%rax"  >> $filename	# sys_exit has code 60
+echo -e "\txor\t\t%rdi,%rdi" >> $filename	# exit code 0
+echo -e "\tsyscall"          >> $filename
